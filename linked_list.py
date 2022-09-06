2c5841faaca830b63b706750eddc6aaf10e1222d
Vishnuhari-17 <vishnuhari.anup@gmail.com>
2022-09-07 00:03:36 +0530

    Added code

--- linked_list.py
+++ linked_list.py
@@ -109,6 +109,19 @@ class LinkedList:
                 current = current.next_node
         return current
 
+    def node_at_index(selfself, index):
+        if index == 0:
+            return self.head
+        else:
+            current = self.hhead
+            position = 0
+
+            while position < index:
+                current = current.next_node
+                position += 1
+
+            return current
+
     def __repr__(self):
         """
         Return a string representation of the list
