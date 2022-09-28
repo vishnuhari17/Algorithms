4056279721bd0b84776a10586ee62f90af528639
Vishnuhari-17 <vishnuhari.anup@gmail.com>
2022-09-28 22:38:02 +0530

    Added code

--- integer_to_roman.py
+++ integer_to_roman.py
@@ -1,15 +1,9 @@
 num=int(input("Enter Number : "))
-div = 1
 value = ""
-inte = {1:"I",5: "V",10: "X",50: "L", 100: "C",500: "D",1000: "M"}
-while num >= 10*div:
-    div *= 10
-print(div)
-while num:
-    digit = num//div
-    print("Digit : ",digit)
-    r = str(value)+str(inte.get(digit))
-    print (value)
-    num = num % div
-    div /= 10
+integer = [["I",1],["IV",4],["V",5],["IX",9],["X",10],["XL",40],["L",50],["XC",90], ["C",100],["CD",400],["D",500],["CM",900],["M",1000]]
+for sym, val in reversed(integer):
+    if num//val:
+        count = num//val
+        value += (sym*count)
+        num = num % val
 print(value)
--- roman_to_integer.py
+++ roman_to_integer.py
@@ -1,16 +1,9 @@
 s=input("Enter the roman number : ")
 value = 0
-i=0
 roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-while i < len(s):
-    if i + 1 < len(s):
-        if roman.get(s[i]) < roman.get(s[i + 1]):
-            value += (roman.get(s[i + 1]) - roman.get(s[i]))
-            i+=2
-        else:
-            value += roman.get(s[i])
-            i+=1
+for i in range(len(s)):
+    if i+1 < len(s) and roman[s[i]] < roman[s[i+1]]:
+        value -= roman[s[i]]
     else:
-        value += roman.get(s[i])
-        i+=1
+        value += roman[s[i]]
 print(value)
